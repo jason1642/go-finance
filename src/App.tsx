cf742deb9e04690056328e1b94cbbcf75c217960
jason1642 <jason1642@gmail.com>
2023-01-16 06:06:23 -0500

    .net backend api w/Mongodb

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import Header from './components/header/Header'
 import './App.css';
 import HomeSearchBar from './components/search-bar/HomeSearchBar';
 import MainRoutes from './routes/main-routes';
+import axios from 'axios'
 
 
+const App = () =>{
 
-function App () {
+  React.useEffect(()=>{
+    axios.get('https://localhost:7025/api/Users').then(res=>{
+      console.log(res.data)
+    })
+  },[])
+
   return (
     <div className="App">
        <Header />
