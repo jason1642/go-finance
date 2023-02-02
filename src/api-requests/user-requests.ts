0a024d63f4652033a5a8f48184fd78a917189bb6
jason1642 <jason1642@gmail.com>
2023-02-02 12:04:58 -0500

    base url updates aws

--- src/api-requests/user-requests.ts
+++ src/api-requests/user-requests.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'production' ?
-    'https://net-finance.netlify.app/api' 
+    'https://main.d1pbrktrl7a0d8.amplifyapp.com/api' 
     :'https://localhost:7025/api',
     withCredentials: true,
 })
