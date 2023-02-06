8ed782e5a5d1da206b13db0107c8e3518b30fa9f
jason1642 <jason1642@gmail.com>
2023-02-06 12:34:06 -0500

    Update Feed.tsx

--- src/components/home-page/RightColumn/Feed.tsx
+++ src/components/home-page/RightColumn/Feed.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import FeedHeader from './FeedHeader'
 import FeedMain from './FeedMain'
-import axios from 'axios'
+// import axios from 'axios'
   const Container = styled.div`
   `;
 
@@ -13,13 +13,13 @@ interface ComponentProps {
 const Feed: React.FunctionComponent<ComponentProps> = () => {
 
 
-  const [newsApiData, setNewsApiData] = useState([])
-
+  // const [newsApiData, setNewsApiData] = useState([])
+const newsApiData : any[]= []
   useEffect(() => {
     // const NEWSAPI_KEY = 'cce7827002dc4aacac0aa05b70ad0f33'
     // const NEWSAPI_KEY_TWO = '1fae1394978c433199f7c1279bd1cae4'
 
-    const BING_NEWS_API_KEY = '99c7685635msh8ab852673ebd9c8p11222bjsnf6f124405f1c'
+    // const BING_NEWS_API_KEY = '99c7685635msh8ab852673ebd9c8p11222bjsnf6f124405f1c'
 
     // const fetchNewsData = async () => {
     //   const response = await axios.get(`https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=${NEWSAPI_KEY_TWO}`)
